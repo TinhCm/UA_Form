8129ea4737a30740fb98481fd862f4dfaa419d76
TinhCM <duongvantinh07072015@gmail.com>
2023-04-05 00:34:08 +0700

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,22 @@ import { TreeNode } from 'primeng/api';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  sales!: TreeNode[];
-  cols!: any[];
-  ngOnInit(): void {
-    this.sales = [
-      {
-        data: {
-          id: 1,
-          date: '4/4/2023',
-          chuyen: 1,
-          maHang: 'KHKHK1',
-          mau: 'red',
-          loaiChuyenDoi: 1,
-          startTime: '4/4/2023',
-          one: 9,
-          two: 9,
-          three: 9,
-          four: 9,
-          fire: 9,
-        },
-      },
-    ];
-  }
+  products = [
+    {
+      id: 1,
+      date: '4/4/2023',
+      chuyen: 1,
+      maHang: 'KHKHK1',
+      mau: 'red',
+      loaiChuyenDoi: 1,
+      startTime: '4/4/2023',
+      one: 9,
+      two: 9,
+      three: 9,
+      four: 9,
+      fire: 9,
+    },
+  ];
+  ngOnInit(): void {}
   title = 'UA_Form';
 }
