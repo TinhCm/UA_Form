b765a920b82cba7d8fc211b26f17a6dd5f4784ef
Audit Intern <intern.audit@un-available.net>
2023-04-04 16:40:25 +0700

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  sales!: TreeNode[];
+  cols!: any[];
+  ngOnInit(): void {
+    this.sales = [
+      {
+        data: {
+          id: 1,
+          date: '4/4/2023',
+          chuyen: 1,
+          maHang: 'KHKHK1',
+          mau: 'red',
+          loaiChuyenDoi: 1,
+          startTime: '4/4/2023',
+          one: 9,
+          two: 9,
+          three: 9,
+          four: 9,
+          fire: 9,
+        },
+      },
+    ];
+  }
   title = 'UA_Form';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienBanDanhGiaComponent } from './bien-ban-danh-gia/bien-ban-danh-gia.component';
 
+// Route configuration
+import { Routes, RouterModule } from '@angular/router';
+const appRouter: Routes = [
+  {
+    path: 'bien-ban-danh-gia',
+    component: BienBanDanhGiaComponent,
+  },
+];
+
+// Library configuration
+import { TreeTableModule } from 'primeng/treetable';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { TableModule } from 'primeng/table';
+import { TreeTableService } from 'primeng/treetable';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BienBanDanhGiaComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRouter),
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    // MatPaginatorModule,
+    TableModule,
+    TreeTableModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [TreeTableModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
