559093192d4be186959eca3f0479e52c8b3eaf7a
TinhCM <duongvantinh07072015@gmail.com>
2023-04-05 00:36:00 +0700

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BienBanDanhGiaComponent } from './bien-ban-danh-gia/bien-ban-danh-gia.component';
 
 // Route configuration
 import { Routes, RouterModule } from '@angular/router';
-const appRouter: Routes = [
-  {
-    path: 'bien-ban-danh-gia',
-    component: BienBanDanhGiaComponent,
-  },
-];
 
 // Library configuration
 import { TreeTableModule } from 'primeng/treetable';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { TableModule } from 'primeng/table';
-import { TreeTableService } from 'primeng/treetable';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 @NgModule({
-  declarations: [AppComponent, BienBanDanhGiaComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRouter),
     MatTableModule,
     FormsModule,
     MatInputModule,
-    // MatPaginatorModule,
     TableModule,
     TreeTableModule,
+    InputTextModule,
+    DropdownModule,
   ],
   providers: [TreeTableModule],
   bootstrap: [AppComponent],
